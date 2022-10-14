aa18dc2930e6c2aab7eed74052c731816ca730e7
DuAlexandre <luis.edu.alex@gmail.com>
2022-10-14 20:12:09 -0300

    Create index route

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,26 @@
 import express from "express";
+import { Routes } from "./interfaces/routes.interface";
 
-export default class App {
+class App {
     public app: express.Application;
+    public port: string | number
 
-    constructor() {
+    constructor(routes: Routes[]) {
         this.app = express()
+        this.port = 3000
+
+        this.initializeDependencies()
     }
 
     public listen() {
-        this.app.listen(3000, () => {
-            console.log("Servidor rodando na porta 3000.");
+        this.app.listen(this.port, () => {
+            console.log(`App listening on the port ${this.port}`);
         })
     }
+
+    private initializeDependencies() {
+        this.app.use(express.json())
+    }
 }
+
+export default App
